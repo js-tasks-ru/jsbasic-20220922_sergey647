e194d6725761b275cc5ee2b69e283bd6864ecfd6
sbugs@mail.ru <sbugs@mail.ru>
2022-10-15 16:03:26 +0300

    Module 7, task 2

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,12 +1,12 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
-  #title;
-  #modalBody;
   constructor() {
-    let me = this;
-    this.#title = '';
-    this.#modalBody = '';
+    this.eventListener = e => {
+      if (e.code === "Escape") {
+        this.close();
+      }
+    };
     this.elem = createElement(`
       <div class="modal">
         <div class="modal__overlay"></div>
@@ -21,36 +21,25 @@ export default class Modal {
         </div>
       </div>
     `);
-    const removeFunc = function() {
-      me.elem.remove();
-    }
-    const removeFuncKey = function(e) {
-      if(e.code === "Escape") {
-        me.elem.remove();
-      }
-    }
     const overlay = this.elem.querySelector('.modal__overlay');
-    overlay.addEventListener('click', removeFunc);
-    document.body.addEventListener('keydown', (e)=>{
-      removeFuncKey(e);
-      document.body.removeEventListener('keydown', removeFuncKey)
-    });
-
     const closeBtn = this.elem.querySelector('.modal__close');
-    closeBtn.addEventListener('click', removeFunc);
+    overlay.addEventListener('click', () => this.close());
+    closeBtn.addEventListener('click', () => this.close());
+    document.body.addEventListener('keydown', this.eventListener );
   }
   open() {
     document.body.append( this.elem );
     document.body.classList.add("is-modal-open");
   }
   setTitle(title) {
-    this.elem.querySelector(".modal__title").innerHTML = title;
+    this.elem.querySelector(".modal__title").textContent = title;
   }
   setBody(body) {
-    console.log(body);
     this.elem.querySelector(".modal__body").append(body);
   }
   close() {
+    this.elem.remove();
     document.body.classList.remove("is-modal-open");
+    document.body.removeEventListener('keydown', this.eventListener);
   }
 }
