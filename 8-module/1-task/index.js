6a80ac8f6a98f54abed3e8077aed628b4bdc64c5
sbugs@mail.ru <sbugs@mail.ru>
2022-11-24 21:52:18 +0300

    Module 6, task 1

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -14,7 +14,6 @@ export default class CartIcon {
   update(cart) {
     if (!cart.isEmpty()) {
       this.elem.classList.add('cart-icon_visible');
-
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
@@ -38,7 +37,47 @@ export default class CartIcon {
     window.addEventListener('resize', () => this.updatePosition());
   }
 
-  updatePosition() {
-    // ваш код ...
+  updatePosition = () => {
+    let cartIcon = document.querySelector('.cart-icon');
+    cartIcon.style.zIndex = '1000';
+    let c = cartIcon.getBoundingClientRect();
+    let leftIndent = Math.min(
+      document.querySelector('.container').getBoundingClientRect().right + 20,
+      document.documentElement.clientWidth - this.elem.offsetWidth - 10
+    );
+    if(!this.isHidden(cartIcon)) {
+      if(c.top < 0) {
+        if(cartIcon.style.position !== 'fixed') {cartIcon.style.position = 'fixed';}
+        if(window.pageYOffset > c.top) {
+          cartIcon.style.left = leftIndent + 'px';
+        }
+      } else {
+        if(window.pageYOffset > c.top) {
+          if(cartIcon.style.position === 'fixed') {
+            cartIcon.style.left = leftIndent + 'px';
+          }
+        } else {
+          if(cartIcon.style.position === 'fixed') {// схлопывание назад
+            cartIcon.style.position = 'absolute';
+            cartIcon.style.left = '';
+          }
+        }
+      }
+    }
+
+    let isMobile = document.documentElement.clientWidth <= 767;
+    if (isMobile) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    }
+  }
+
+  isHidden(elem) {
+    return !elem.offsetWidth || !elem.offsetHeight;
+
   }
 }
