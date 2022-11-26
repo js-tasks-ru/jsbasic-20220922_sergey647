4960711fe757ce66cb3aedff67e9f4651c91d8f2
sbugs@mail.ru <sbugs@mail.ru>
2022-11-26 23:31:54 +0300

    Module 8, task 2

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,30 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner" />
+      </div>
+    `);
+    this.grid = this.elem.querySelector('.products-grid__inner');
+    this.render();
+  }
+  render() {
+    this.grid.innerHTML = '';
+    this.products.map((product) => {
+      let skip = 0;
+      if(this.filters.noNuts && product.nuts) skip++;
+      if(this.filters.vegeterianOnly && !product.vegeterian) skip++;
+      if(product.spiciness > this.filters.maxSpiciness ) skip++;
+      if(this.filters.category && product.category !== this.filters.category) skip++;
+      if(skip === 0) {
+        let card = new ProductCard(product);
+        this.grid.append(card.elem);
+      }
+    });
+  }
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.render();
   }
 }
